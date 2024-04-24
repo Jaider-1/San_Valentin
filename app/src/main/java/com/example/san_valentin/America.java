7d0d1703bfdec8ba57a01286e9d0e6af9e276ef7
lobal <lobalekay19@gmail.com>
2024-04-23 23:44:59 -0500

    Xml de flores terminado con correccones

--- app/src/main/java/com/example/san_valentin/America.java
+++ app/src/main/java/com/example/san_valentin/America.java
@@ -1,6 +1,9 @@
 package com.example.san_valentin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,14 @@ public class America extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void onClick(View view){
+        final int imgBtn1=R.id.imageButton8;
+        Intent _volver=null;
+        if(view.getId()==imgBtn1){
+            _volver=new Intent(America.this, MainActivity.class);
+            Toast.makeText(this,"flores_del_mundo", Toast.LENGTH_SHORT).show();
+            startActivity(_volver);
+        }
     }
 }
