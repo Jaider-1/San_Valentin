1956e3d9608d98e3328bc64bccbdf492e838c881
lobal <lobalekay19@gmail.com>
2024-04-23 23:33:40 -0500

    Xml de flores terminado con correccones

--- app/src/main/java/com/example/san_valentin/MainActivity.java
+++ app/src/main/java/com/example/san_valentin/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.san_valentin;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        imageButton4=(ImageButton) findViewById(R.id.imageButton4);
+
     }
+   public void onClick(View view){
+        final int imgBtm_1=R.id.imageButton4;
+       Intent myIntent;
+       myIntent=null;
+       if (view.getId()==imgBtm_1){
+           myIntent=new Intent(MainActivity.this, flores_del_mundo.class);
+           Toast.makeText(this,"flores_del_mundo", Toast.LENGTH_SHORT).show();
+
+       }
+       startActivity(myIntent);
+   }
+
 }
